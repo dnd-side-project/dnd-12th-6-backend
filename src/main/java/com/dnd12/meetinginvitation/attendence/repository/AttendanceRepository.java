f10e7e8a257367009946c3fc75c8aa5f3f9cc956
seonghuncode <gnos456@naver.com>
2025-03-22 02:11:25 +0900

    feature/회원응답 수정 API추가

--- src/main/java/com/dnd12/meetinginvitation/attendence/repository/AttendanceRepository.java
+++ src/main/java/com/dnd12/meetinginvitation/attendence/repository/AttendanceRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
@@ -12,4 +13,5 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     boolean existsByName(String name);
     boolean existsByUser_Id(Long userId);
     Attendance findByInvitationIdAndName(Long invitationId, String name);
+    Optional<Attendance> findByInvitationIdAndUserId(Long invitationId, Long userId);
 }
